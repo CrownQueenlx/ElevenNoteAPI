de74de27c5c81a73be131d37f7062698e2f2dd65
Brianne W <brianne4god@gmail.com>
2023-06-01 11:28:32 -0400

    Init UserService interface and class

---
+++ ElevenNote.Services/User/UserService.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Services.User
+{
+    public class UserService : IUserService
+    {
+    }
+}
